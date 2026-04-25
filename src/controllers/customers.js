b980a508293aa086b09779d66822e0d9179b1cf4
ShimiVi <shimshonvi123@gmail.com>
2026-04-25 15:12:15 +0300

    add validation and Error handling : customers

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -15,6 +15,9 @@ const getById = async (req, res) => {
     try{
         const {id} = req.params;
         const customers = await customersService.getById(id); 
+        if(!customers){
+            return res.status(404).json({message: 'Customers not found'}); 
+        }
         res.json(customers); 
     }
     catch(error){
@@ -25,6 +28,19 @@ const getById = async (req, res) => {
 const create = async (req, res)=>{
     try{
         const {name,phone,email} = req.body; 
+
+        const errors = {}; 
+
+        if(!name){
+            errors.name = 'Name is required'; 
+        }
+        if(!phone){
+            errors.phone = 'Phone is required'; 
+        }
+
+        if(Object.keys(errors).length >0){
+            return res.status(400).json({errors}); 
+        }
         const customers = await customersService.create(name,phone,email); 
         res.status(201).json(customers);
     }
@@ -37,7 +53,24 @@ const update = async (req, res) => {
     try{
         const{id} = req.params;
         const{name,phone,email} = req.body;
+
+        const errors = {}; 
+
+        if(!name){
+            errors.name = 'Name is required'; 
+        }
+        if(!phone){
+            errors.phone = 'Phone is required'; 
+        }
+
+        if(Object.keys(errors).length >0){
+            return res.status(400).json({errors}); 
+        }
+
         const customers = await customersService.update(id,name,phone,email);
+        if(!customers){
+            return res.status(404).json({message: 'Customer not found'});
+        }
         res.status(200).json(customers); 
     }
     catch(error){
