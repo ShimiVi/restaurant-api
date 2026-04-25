e2ae9ab69cd329237f5c1b3487cee550e7aa9959
ShimiVi <shimshonvi123@gmail.com>
2026-04-25 15:28:17 +0300

    add validation and error handling : orders

--- src/controllers/orders.js
+++ src/controllers/orders.js
@@ -14,6 +14,9 @@ const getById = async(req,res) =>{
     try{
         const {id} = req.params;
         const orders = await ordersService.getById(id);
+        if(!orders){
+            return res.status(404).json({message: 'Order not found'}); 
+        }
         res.json(orders); 
     }
     catch(error){
@@ -24,6 +27,19 @@ const getById = async(req,res) =>{
 const create = async(req,res) => {
     try{
         const{customer_id, items} =req.body; 
+
+        const errors = {};
+        if(!customer_id){
+            errors.customer_id = 'Customer is required';
+        }
+
+        if(!items || !Array.isArray(items) || items.length === 0){
+            errors.items ='items must be a non-empty array'; 
+        }
+
+        if(Object.keys(errors).length > 0){
+            return res.status(400).json({errors}); 
+        }
         const orders = await ordersService.create(customer_id, items); 
         res.status(201).json(orders);
     }
@@ -36,7 +52,13 @@ const updateStatus = async(req,res) =>{
     try{
         const{id} = req.params;
         const{status}=req.body;
+        if(!status){
+            return res.status(400).json({message: 'status is required'});
+        }
         const orders = await ordersService.updateStatus(id,status);
+        if(!orders){
+            return res.status(404).json({message: 'orders not found'});
+        }
         res.status(200).json(orders); 
     }
     catch(error){
