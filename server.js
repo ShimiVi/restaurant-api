279d132fef10c763fb24c2f0d20710309b2ff296
ShimiVi <shimshonvi123@gmail.com>
2026-04-27 18:12:13 +0300

    add auth: middleware verifyToken and checkRole, JWT integration

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const categoriesRouter = require('./src/routes/categories');
 const menuitemsRouter = require('./src/routes/menuItems'); 
 const customersRouter = require('./src/routes/customers');
 const ordersRouter = require('./src/routes/orders'); 
+const authRouter = require('./src/routes/auth');
 
 const app = express(); 
 app.use(express.json()); 
@@ -11,6 +12,7 @@ app.use('/categories',categoriesRouter);
 app.use('/menu-items',menuitemsRouter);
 app.use('/customers',customersRouter);
 app.use('/orders', ordersRouter); 
+app.use('/auth', authRouter); 
 
 const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () =>{
